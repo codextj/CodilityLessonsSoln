d3a403f11cf3104d1114d45bb990421d43744c06
codextj <tanmayjain.cs@gmail.com>
2017-09-04 18:07:30 +0530

    Create CyclicRotation.java

---
+++ CyclicRotation.java
@@ -0,0 +1,13 @@
+public int[] solution(int[] A, int K)
+    {
+        
+        int size = A.length;
+        if(size == 0 || K % size ==0){
+        return A; //rotatedArray will be same as orignal
+        }
+        int[] rotatedArray = new int[A.length];
+        for(int i = 0; i < A.length; i++) {
+            rotatedArray[(i + K) % A.length] = A[i];
+        }
+        return rotatedArray;
+   }  
