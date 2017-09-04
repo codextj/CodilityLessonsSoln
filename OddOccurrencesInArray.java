c2ddef5812eb8fdbcf5ba33af93e2e0b5a863ec6
codextj <tanmayjain.cs@gmail.com>
2017-09-04 17:55:34 +0530

    Create OddOccurrencesInArray.java

---
+++ OddOccurrencesInArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    
+    public int solution(int[] A) {
+    Arrays.sort(A);
+    int num = 0;
+    int countForThisNum =0;
+    int OddTimeOcuuringNum =0;
+    
+    for(int i =0; i<A.length; i++){
+        num = A[i];
+        while(i <A.length && num ==A[i]){
+            countForThisNum++;
+            i++;
+            }
+        if(countForThisNum % 2 !=0)
+        {   
+            OddTimeOcuuringNum = num;
+            break;
+        }
+        else{
+             i--;
+            }
+    }
+    return OddTimeOcuuringNum;
+}
+}
